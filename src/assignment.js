22fb4ab593cf4f130eff1b272e9ce3750eff932f
Taiwo Ojo <98031422+Folarh@users.noreply.github.com>
2022-07-03 13:26:11 +0000

    third attempt

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,19 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let numbers= 0;
+  for(i=0; i< arrayOfNumbers.length; ++i){
+    numbers += arrayOfNumbers[i]
+  }
+
+    return numbers
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+
+
+
+
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +38,10 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let evenNumbers=arrayOfNumbers.filter(a=>a%2===0)
+    return evenNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
